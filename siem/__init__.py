9d70f576234a5c9c4e59c540f19d4b61b235a4e4
twisted <billy@jecks.co.uk>
2026-05-03 13:15:17 +0100

    update: init file cleanup

--- siem/__init__.py
+++ siem/__init__.py
@@ -1,7 +1,1 @@
 # siem/__init__.py
-
-from siem.wazuh_forwarder import forward_alert
-
-__all__ = [
-    "forward_alert",
-]
--- utils/__init__.py
+++ utils/__init__.py
@@ -1,17 +1,1 @@
 # utils/__init__.py
-
-from utils.config_loader import get_config, set_config_path
-from utils.logger import get_logger
-from utils.progress import tqdm_bar, TrainingSpinner
-from utils.gpu_utils import setup_gpu, release_gpu_memory, cuml_available
-
-__all__ = [
-    "get_config",
-    "set_config_path",
-    "get_logger",
-    "tqdm_bar",
-    "TrainingSpinner",
-    "setup_gpu",
-    "release_gpu_memory",
-    "cuml_available",
-]
